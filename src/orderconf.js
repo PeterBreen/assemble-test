11abc8873a95b9fe5eabfb2d92288b2bc3d129ec
Peter Breen <pbreen@gmail.com>
2019-04-20 14:39:48 -0700

    add basic components for cart, review, confirm pages

---
+++ src/orderconf.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class OrderConf extends React.Component {
+  render() {
+    return (
+      <p>I'm an order confirmation page!</p>
+    )
+  }
+}
+
+export default OrderConf;
